8703b12c518122eb57a650a7886804f13034e474
Muh. Firdaus <muhfirdaus0805@gmail.com>
2024-04-03 13:38:19 +0800

    update userRoute

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,1 +1,55 @@
-v;
+import express from 'express';
+import {
+  setUser,
+  setActivateUser,
+  getUser,
+  getUserById,
+  setLogin,
+  setRefreshToken,
+  updateUser,
+  avatarUser,
+  deleteUser,
+  forgotPassword,
+  favorite,
+  getAllFavoriteUser,
+  sendMessage,
+  removeUserAccount,
+  deleteUserByAdmin,
+  isLoggedIn,
+} from '../controllers/userController.js';
+import { autenticate } from '../controllers/errorHandlingController.js';
+import { upload } from '../middleware/multer_firebase.js';
+const userRouter = express.Router();
+
+userRouter.post('/users', setUser);
+userRouter.get('/users/refresh', setRefreshToken);
+userRouter.get('/admin/users', autenticate, getUser); //admin get all user
+userRouter.get('/users', autenticate, getUserById);
+userRouter.get('/users/me', autenticate, isLoggedIn);
+userRouter.get('/users/activate/:id', setActivateUser);
+userRouter.post('/users/login', setLogin);
+
+userRouter.post('/admin/login', setLogin); //admin
+userRouter.get('/admin/refresh', setRefreshToken); //admin
+
+userRouter.patch('/users', autenticate, updateUser);
+userRouter.patch(
+  '/users/avatar',
+  autenticate,
+  upload.single('avatar'),
+  avatarUser
+);
+userRouter.delete('/users/:id', autenticate, deleteUser);
+
+userRouter.delete('/admin/users/:id', autenticate, deleteUserByAdmin); //admin
+
+userRouter.post('/users/forgot-password', forgotPassword);
+
+userRouter.get('/users/favorite/all', autenticate, getAllFavoriteUser);
+userRouter.post('/users/favorite', autenticate, favorite);
+
+userRouter.post('/send-message', sendMessage);
+
+userRouter.delete('/users', autenticate, removeUserAccount);
+
+export default userRouter;
